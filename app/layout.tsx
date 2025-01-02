053d8430cbe697bf2b8d1a370d2162a88c6e4045
Engraya <ahmadyakubuaya@gmail.com>
2025-01-02 13:46:58 +0100

    Added some files

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/layout/navbar"
 import Footer from "@/components/layout/footer"
 import { ThemeProvider } from "@/components/theme-provider"
 import { settings } from "@/config/settings"
-
+import { Toaster } from "@/components/ui/toaster"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
@@ -79,6 +79,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <Navbar />
             {children}
             <Footer />
+            <Toaster />
           </ThemeProvider>
         )}
       </body>
